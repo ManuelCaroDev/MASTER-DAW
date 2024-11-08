3d0bd15f2a9a27b7eef0f24b3d949872b690dff1
ManuelCaroDev <manuelcaro.dev@gmail.com>
2024-11-08 19:28:50 +0100

    javaScrip

---
+++ 0005.javaScript/07_11_24/1.intro.js
@@ -0,0 +1,20 @@
+/* var movie;
+var serie = "dune"
+console.log(movie); */
+/*
+esto es para comentar en bloque
+*/
+//ahora voy a asignarle un valor
+/* movie = "The Matrix";
+console.log(movie);
+ */
+const movie = "The matrix" //codigo a nivel global --- const --> constante no cambia de valor
+let serie = "Dune" // se lee desde dentro de local y desde global, pero desde global a dentro de local no se lee
+
+
+
+
+if(true){
+    /* var movie //codigo a nivel local */
+
+}
---
+++ 0005.javaScript/07_11_24/2.tiposdedatos.js
@@ -0,0 +1,28 @@
+//REGLAS
+const firstName = "Mongolo"
+
+//String
+const str = "Hola soy un string; 1,5"
+const str2 = 'hola soy una "string"'
+//Numbre
+const num = 1
+const num2 = 13.5
+//Arrays
+const lisAnimales = ["perro", "loro", "oso"]
+const listNumeros = [5, 8, 15.8, 115]
+//Objetos
+const batman = {
+    name: "Batman",
+    realName: "bruce wayne",
+    city: "Gotham city",
+    age: 40,
+    family: [],
+
+
+};
+//booleans
+const boolean = false;
+//null
+const nulo = null;
+//Undefined
+const noDefinido = undefined;
